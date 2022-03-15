1e3ec9700171c33092cb9ee9d5b2ac544a559ed6
bisercode <53227912+bisercode@users.noreply.github.com>
2022-03-15 19:06:38 +0100

    IT added

---
+++ basicejb-test/src/test/java/org/myorg/basicejb/earejb/ReservationIT.java
@@ -0,0 +1,26 @@
+package org.myorg.basicejb.earejb;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReservationIT {
+    private static final Logger logger = LoggerFactory.getLogger(ReservationIT.class);
+    private InitialContext jndi;
+
+    @Before
+    public void setUp() throws NamingException {
+        logger.debug("getting jndi initial context");
+        jndi = new InitialContext();
+        logger.debug("jndi={}", jndi.getEnvironment());
+    }
+
+    @Test
+    public void testPing() {
+        logger.info("*** testPing ***");
+    }
+}
